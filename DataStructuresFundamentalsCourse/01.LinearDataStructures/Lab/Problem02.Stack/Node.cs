197ee1e133ed453829d934fbe82ec7e8147fc3ca
Milcho Arnaudov <37160354+milchoarnaudov@users.noreply.github.com>
2020-10-02 16:03:00 +0300

    Update Node.cs

--- DataStructuresFundamentalsCourse/01.LinearDataStructures/Lab/Problem02.Stack/Node.cs
+++ DataStructuresFundamentalsCourse/01.LinearDataStructures/Lab/Problem02.Stack/Node.cs
@@ -11,10 +11,5 @@
 
         public T Value { get; set; }
         public Node<T> Next { get; set; }
-
-        public void Add(Node<T> nextElement)
-        {
-            this.Next = nextElement;
-        }
     }
 }
