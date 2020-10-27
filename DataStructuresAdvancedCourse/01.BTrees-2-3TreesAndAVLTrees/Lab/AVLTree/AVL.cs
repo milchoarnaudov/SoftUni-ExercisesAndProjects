ae8f77a957ea773154aba6a968e6c85909b5b9dd
Milcho <milchoarnaudov9807@abv.bg>
2020-10-27 11:30:17 +0200

    Add AVL Lab

---
+++ DataStructuresAdvancedCourse/01.BTrees-2-3TreesAndAVLTrees/Lab/AVLTree/AVL.cs
@@ -0,0 +1,155 @@
+﻿namespace AVLTree
+{
+    using System;
+
+    public class AVL<T> where T : IComparable<T>
+    {
+        public Node<T> Root { get; private set; }
+
+        public bool Contains(T item)
+        {
+            var node = this.Search(this.Root, item);
+            return node != null;
+        }
+
+        public void Insert(T item)
+        {
+            this.Root = this.Insert(this.Root, item);
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            this.EachInOrder(this.Root, action);
+        }
+
+        private Node<T> Insert(Node<T> node, T item)
+        {
+            if (node == null)
+            {
+                return new Node<T>(item);
+            }
+
+            int cmp = item.CompareTo(node.Value);
+            if (cmp < 0)
+            {
+                node.Left = this.Insert(node.Left, item);
+            }
+            else if (cmp > 0)
+            {
+                node.Right = this.Insert(node.Right, item);
+            }
+
+            node = Balance(node);
+            UpdateHeight(node);
+
+            return node;
+        }
+
+        private Node<T> Search(Node<T> node, T item)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            int cmp = item.CompareTo(node.Value);
+            if (cmp < 0)
+            {
+                return Search(node.Left, item);
+            }
+            else if (cmp > 0)
+            {
+                return Search(node.Right, item);
+            }
+
+            return node;
+        }
+
+        private void EachInOrder(Node<T> node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(node.Left, action);
+            action(node.Value);
+            this.EachInOrder(node.Right, action);
+        }
+
+        private Node<T> Balance(Node<T> node)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            var balanceIndex = Height(node.Left) - Height(node.Right);
+
+            if (balanceIndex > 1)
+            {
+                var childBalanceIndex = Height(node.Left.Left) - Height(node.Left.Right);
+
+                if (childBalanceIndex < 0)
+                {
+                    node.Left = RotateLeft(node.Left);
+                }
+
+                node = RotateRight(node);
+            }
+            else if (balanceIndex < -1)
+            {
+                var childBalance = Height(node.Right.Left) - Height(node.Right.Right);
+
+                if (childBalance > 0)
+                {
+                    node.Right = RotateRight(node.Right);
+                }
+
+                node = RotateLeft(node);
+            }
+
+            return node;
+        }
+
+        private Node<T> RotateRight(Node<T> node)
+        {
+            var left = node.Left;
+            node.Left = left.Right;
+            left.Right = node;
+
+            this.UpdateHeight(node);
+
+            return left;
+        }
+
+        private Node<T> RotateLeft(Node<T> node)
+        {
+            var right = node.Right;
+            node.Right = right.Left;
+            right.Left = node;
+
+            this.UpdateHeight(node);
+
+            return right;
+        }
+
+        private void UpdateHeight(Node<T> node)
+        {
+            if (node != null)
+            {
+                node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+            }
+        }
+
+        private int Height(Node<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return node.Height;
+        }
+    }
+}
