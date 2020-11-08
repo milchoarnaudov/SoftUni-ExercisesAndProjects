a2a1edf5a094b1eea39cd35ed0bef6f6323a93f3
Milcho <milchoarnaudov9807@abv.bg>
2020-11-08 08:56:41 +0200

    Add RedBlackAndAATrees Lab

---
+++ DataStructuresAdvancedCourse/02.Red-BlackAndAATrees/Lab/01.RedBlackTree/Node.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.RedBlackTree
+{
+    using System;
+
+    public partial class RedBlackTree<T> where T : IComparable
+    {
+        private class Node
+        {
+            const bool Red = true;
+            const bool Black = false;
+
+            public Node(T value)
+            {
+                this.Value = value;
+                this.Color = Red;
+            }
+
+            public T Value { get; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public bool Color { get; set; }
+            public int Count { get; set; }
+        }
+
+    }
+}
