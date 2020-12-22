022c26cb332a1bc84a085b219afdda937c47317f
Milcho <milchoarnaudov9807@abv.bg>
2020-12-22 17:48:16 +0200

    Update Microsystem exercise

---
+++ DataStructuresAdvancedCourse/05.ExamPreparation/Microsystem/01.Microsystem/PriceComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace _01.Microsystem
+{
+    public class PriceComparer : IComparer<Computer>
+    {
+        public int Compare(Computer x, Computer y)
+        {
+            return y.Price.CompareTo(x.Price);
+        }
+    }
+}
